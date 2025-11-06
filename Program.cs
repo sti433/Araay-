de71aafeff54b99ea95937e44d6e97c82f0b265b
sti433 <sti.ri433@gmail.com>
2025-11-06 20:28:21 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_pro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[15];
+            int i;
+            for (i = 0; i < arr.Length; i++)
+            {
+
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
